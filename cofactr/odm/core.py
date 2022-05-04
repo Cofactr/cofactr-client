5343eb25ac34c220068fb8e5cd5ac8fb644d6b99
notrueblood <notrueblood@gmail.com>
2022-05-04 11:57:53 -0400

    Add part access by cpid

--- cofactr/odm/core.py
+++ cofactr/odm/core.py
@@ -1,7 +1,43 @@
-#search function
-    #search but returns part objects
+"""Core functionality."""
+# Standard Modules
+from typing import List
 
-#bulk fetch function
-    #accepts a list of cpids
-    #gets all the data in a single lookup for those cpids
-    #returns part objects
+# Local Modules
+from cofactr.graph import GraphAPI
+from cofactr.odm.part import Part
+
+
+def search_parts(query: str, limit=10, external=True) -> List[Part]:
+    """Search for parts."""
+
+    graph = GraphAPI()
+
+    cursor = graph.get_products(
+        query=query,
+        fields="id,statements{spec,image,mfr,mpn,desc,package,numberofpins},offers",
+        batch_size=100,
+        limit=limit,
+        external=external,
+        render=False,
+    )
+
+    return [Part(**data) for data in cursor]
+
+
+def get_part(cpid: str, external=True) -> List[Part]:
+    """Search for parts."""
+
+    graph = GraphAPI()
+
+    product = graph.get_product(
+        id=cpid,
+        fields="id,statements{spec,image,mfr,mpn,desc,package,numberofpins},offers",
+        external=external,
+        render=False,
+    )
+
+    return Part(**product["data"]) if product else None
+
+
+# TODO: Add a bulk fetch function that accepts a list of cpids
+# and gets all the data in a single request, returning Part objects.
--- tests/odm/test_core.py
+++ tests/odm/test_core.py
@@ -3,45 +3,34 @@
 import pytest
 
 # Local Modules
-from cofactr.graph import GraphAPI
-from cofactr.odm.part import Part
-
-
-from typing import List
-
-
-def search_parts(query: str, limit=10, external=True) -> List[Part]:
-    """Search for parts."""
-
-    graph = GraphAPI()
-
-    cursor = graph.get_products(
-        query=query,
-        fields="id,statements{spec,image,mfr,mpn,desc,package,numberofpins},offers",
-        batch_size=100,
-        limit=limit,
-        external=external,
-        render=False,
-    )
-
-    return [Part(**data) for data in cursor]
+from cofactr.odm.core import get_part, search_parts
 
 
 @pytest.mark.parametrize(
     "mpn",
     ["IRFH4251DTRPBF"],
 )
-def test_part(mpn: str):
+def test_search_part(mpn: str):
     """Test Part."""
 
     parts = search_parts(query=mpn, limit=1, external=False)
 
     assert len(parts) == 1
 
-    part = parts[0]
+
+@pytest.mark.parametrize(
+    "cpid",
+    ["TRGC72NRRA4W"],
+)
+def test_get_part(cpid: str):
+    """Test Part."""
+
+    part = get_part(cpid=cpid, external=False)
+
+    assert part
 
     assert part.id == "TRGC72NRRA4W"
-    assert part.mpn == mpn
+    assert part.mpn == "IRFH4251DTRPBF"
     assert part.hero_image == "https://assets.cofactr.com/TRGC72NRRA4W/part-img.jpg"
     assert (
         part.description
