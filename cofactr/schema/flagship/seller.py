6f29ce7b570b8f04611888a151b1d9737b616003
notrueblood <notrueblood@gmail.com>
2022-05-11 12:47:04 -0400

    Update schemas

--- cofactr/schema/flagship/seller.py
+++ cofactr/schema/flagship/seller.py
@@ -8,12 +8,13 @@ from typing import List, Optional
 class Seller:  # pylint: disable=too-many-instance-attributes
     """Part seller."""
 
+    id: str
     label: str
     aliases: List[str]
     authenticity_score: Optional[int]
     availability_score: Optional[int]
     additional_markup: Optional[float]
     additional_fee: Optional[float]
-    certifications: List
+    certifications: List[str]
     #  A separate flag that's independent from accuracy.
-    is_buyable: Optional[bool]
+    is_buyable: bool
