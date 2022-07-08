5e76511d99abadd86f314710c4a01d21ded00545
riggy <riley@cofactr.comriley@sleepy.fish>
2022-07-07 20:04:14 -0400

    added safegaurd against key errors

--- cofactr/graph.py
+++ cofactr/graph.py
@@ -225,7 +225,7 @@ class GraphAPI:  # pylint: disable=too-many-instance-attributes
 
         extracted_product_map = {p.id: p for p in extracted_products}
 
-        products = {id_: extracted_product_map[id_] for id_ in ids}
+        products = {id_: extracted_product_map[id_] for id_ in ids if id_ in extracted_product_map}
 
         return products
 
