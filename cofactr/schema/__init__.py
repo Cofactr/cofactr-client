bc02069c3164a8b2ae05e7abf20389c621fb506d
riggy <riley@cofactr.comriley@sleepy.fish>
2023-03-23 18:01:52 -0400

    added additional flagship product schemas

--- cofactr/schema/__init__.py
+++ cofactr/schema/__init__.py
@@ -12,9 +12,10 @@ from cofactr.schema.flagship_v2.offer import Offer as FlagshipV2Offer
 from cofactr.schema.flagship_v2.part import Part as FlagshipV2Part
 from cofactr.schema.flagship_v3.part import Part as FlagshipV3Part
 from cofactr.schema.flagship_v4.part import Part as FlagshipV4Part
+from cofactr.schema.flagship_v5.part import Part as FlagshipV5Part
 from cofactr.schema.flagship_cache_v0.part import Part as FlagshipCacheV0Part
 from cofactr.schema.flagship_cache_v1.part import Part as FlagshipCacheV1Part
-from cofactr.schema.flagship_cache_v1.part import Part as FlagshipCacheV2Part
+from cofactr.schema.flagship_cache_v2.part import Part as FlagshipCacheV2Part
 from cofactr.schema.logistics.part import Part as LogisticsPart
 from cofactr.schema.logistics_v2.part import Part as LogisticsV2Part
 from cofactr.schema.logistics_v2.offer import Offer as LogisticsV2Offer
@@ -25,6 +26,7 @@ from cofactr.schema.flagship.seller import Seller as FlagshipSeller
 from cofactr.schema.price_solver_v0.part import Part as PriceSolverV0Part
 from cofactr.schema.price_solver_v1.part import Part as PriceSolverV1Part
 from cofactr.schema.price_solver_v2.part import Part as PriceSolverV2Part
+from cofactr.schema.price_solver_v3.part import Part as PriceSolverV3Part
 
 
 class ProductSchemaName(str, Enum):
@@ -35,9 +37,11 @@ class ProductSchemaName(str, Enum):
     FLAGSHIP_V2 = "flagship-v2"
     FLAGSHIP_V3 = "flagship-v3"
     FLAGSHIP_V4 = "flagship-v4"
+    FLAGSHIP_V5 = "flagship-v5"
     FLAGSHIP_CACHE_V0 = "flagship-cache-v0"
     FLAGSHIP_CACHE_V1 = "flagship-cache-v1"
     FLAGSHIP_CACHE_V2 = "flagship-cache-v2"
+    FLAGSHIP_CACHE_V3 = "flagship-cache-v3"
     LOGISTICS = "logistics"
     LOGISTICS_V2 = "logistics-v2"
     LOGISTICS_V3 = "logistics-v3"
@@ -45,6 +49,7 @@ class ProductSchemaName(str, Enum):
     PRICE_SOLVER_V0 = "price-solver-v0"
     PRICE_SOLVER_V1 = "price-solver-v1"
     PRICE_SOLVER_V2 = "price-solver-v2"
+    PRICE_SOLVER_V3 = "price-solver-v3"
 
 
 schema_to_product: Dict[ProductSchemaName, Callable] = {
@@ -52,9 +57,10 @@ schema_to_product: Dict[ProductSchemaName, Callable] = {
     ProductSchemaName.FLAGSHIP_V2: FlagshipV2Part,
     ProductSchemaName.FLAGSHIP_V3: FlagshipV3Part,
     ProductSchemaName.FLAGSHIP_V4: FlagshipV4Part,
+    ProductSchemaName.FLAGSHIP_V5: FlagshipV5Part,
     ProductSchemaName.FLAGSHIP_CACHE_V0: FlagshipCacheV0Part,
     ProductSchemaName.FLAGSHIP_CACHE_V1: FlagshipCacheV1Part,
-    ProductSchemaName.FLAGSHIP_CACHE_V1: FlagshipCacheV2Part,
+    ProductSchemaName.FLAGSHIP_CACHE_V2: FlagshipCacheV2Part,
     ProductSchemaName.LOGISTICS: LogisticsPart,
     ProductSchemaName.LOGISTICS_V2: LogisticsV2Part,
     ProductSchemaName.LOGISTICS_V3: LogisticsV3Part,
@@ -62,6 +68,7 @@ schema_to_product: Dict[ProductSchemaName, Callable] = {
     ProductSchemaName.PRICE_SOLVER_V0: PriceSolverV0Part,
     ProductSchemaName.PRICE_SOLVER_V1: PriceSolverV1Part,
     ProductSchemaName.PRICE_SOLVER_V2: PriceSolverV2Part,
+    ProductSchemaName.PRICE_SOLVER_V3: PriceSolverV3Part,
 }
 
 
