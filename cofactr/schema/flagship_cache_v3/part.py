78e9a23c490a31443c5c3df54cda883fcafa073f
sayad-eng <josephsayad1995@gmail.com>
2023-03-29 17:25:41 +0000

    Remove unused import

--- cofactr/schema/flagship_cache_v3/part.py
+++ cofactr/schema/flagship_cache_v3/part.py
@@ -1,7 +1,7 @@
 """Part class."""
 # Standard Modules
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional
 
 # Local Modules
 from cofactr.schema.flagship_cache_v2.part import Part as FlagshipCacheV2Part
