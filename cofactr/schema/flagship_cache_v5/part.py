67561ab1a969f0d3d863ba15e33e381bdc1561ed
josephsayad <josephsayad95@gmail.com>
2023-08-28 11:31:38 -0400

    Fix/Correct type annotation

--- cofactr/schema/flagship_cache_v5/part.py
+++ cofactr/schema/flagship_cache_v5/part.py
@@ -1,6 +1,7 @@
 """Part class."""
 # Standard Modules
 from dataclasses import dataclass
+from typing import Optional
 
 # Local Modules
 from cofactr.schema.flagship_cache_v4.part import Part as FlagshipCacheV4Part
@@ -10,4 +11,4 @@ from cofactr.schema.flagship_cache_v4.part import Part as FlagshipCacheV4Part
 class Part(FlagshipCacheV4Part):  # pylint: disable=too-many-instance-attributes
     """Part."""
 
-    msl: str
+    msl: Optional[str]
